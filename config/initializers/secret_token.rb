b0ced2e7fc642b958e44c076c42be37fd04ceae1
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-03-18 11:57:24 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'b034b615baa64d7d0468548ca5606348af82a396ebf718c33d979bb70f140d42c5f92efc11ae95a7f651e36e1d1be9d8293bafd994e7767d5d9a2f6c9b96f288'
